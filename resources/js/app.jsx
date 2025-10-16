30f87dd6cfe147fb95823a4787076506d6f1f413
Talha Tech <talhatech116@gmail.com>
2025-10-16 22:36:04 +0500

    🚀 github actions successfull

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>Test Automated Deployment Works 🎉</h1>
+      <h1>Automation Successfull 🎉</h1>
       <p>Congratulations 🎉</p>
     </div>
   );
